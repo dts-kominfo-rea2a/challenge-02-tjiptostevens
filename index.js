2023f59dd6ebaa28785974c2c25eae76e6662b29
tjiptostevens <steveneonites@gmail.com>
2022-05-27 21:28:06 +0700

    feat: Challenge 2 - Done

--- index.js
+++ index.js
@@ -14,59 +14,88 @@
 const arrayObjectPegawai = require("./data-customer.json");
 
 function lakukanLooping(arrPegawai) {
-  // ! JANGAN DIMODIFIKASI
-  let dataYangAkanDilooping = arrPegawai;
+    // ! JANGAN DIMODIFIKASI
+    let dataYangAkanDilooping = arrPegawai;
 
-  /*
-    TODO 1: Buatlah sebuah variabel bernama "hasilLooping" 
-      yang berisi gabungan nama depan dan belakang dari masing masing pegawai
+    /*
+        TODO 1: Buatlah sebuah variabel bernama "hasilLooping" 
+          yang berisi gabungan nama depan dan belakang dari masing masing pegawai
 
-      Contoh: ["Aisyah Nirmala", "Mansur Faisal", ...]
-  */
-  let hasilLooping = null;
+          Contoh: ["Aisyah Nirmala", "Mansur Faisal", ...]
+      */
+    let hasilLooping = null;
 
-  /*
-    TODO 2: Buatlah sebuah variabel bernama "jumlahPria"
+    /*
+      TODO 2: Buatlah sebuah variabel bernama "jumlahPria"
       yang berisi jumlah pria dari masing masing pegawai
-  */
-  let jumlahPria = null;
+      */
+    let jumlahPria = null;
 
-  /*
-    TODO 3: Buatlah sebuah variabel bernama "jumlahWanita"
+    /*
+      TODO 3: Buatlah sebuah variabel bernama "jumlahWanita"
       yang berisi jumlah wanita dari masing masing pegawai
-  */
-  let jumlahWanita = null;
+      */
+    let jumlahWanita = null;
+    // change to array
+    hasilLooping = [];
+    for (let i = 0; i < dataYangAkanDilooping.length; i++) {
+        // data Looping
+        let loop = "";
+        loop +=
+            dataYangAkanDilooping[i].namaDepan +
+            " " +
+            dataYangAkanDilooping[i].namaBelakang;
+        // add to array
+        hasilLooping.push(loop);
+        // menghitung jumlah Pria
+        if (dataYangAkanDilooping[i].jenisKelamin === "M") {
+            jumlahPria += 1;
+        }
+        // menghitung jumlah Wanita
+        if (dataYangAkanDilooping[i].jenisKelamin === "F") {
+            jumlahWanita += 1;
+        }
+    }
 
-  /*
-    TODO 4: Buatlah sebuah variabel bernama "komentar"
-      yang akan mengomentari apakah lebih banyak Pria atau Wanita
-      
-      Apabila Pria lebih banyak dari Wanita, maka komentar akan berisi:
-        "Jumlah Pria lebih banyak dari Wanita"
-      Apabila Wanita lebih banyak dari Pria, maka komentar akan berisi:
-        "Jumlah Wanita lebih banyak dari Pria"
-      Apabila imbang, maka komentar akan berisi:
-        "Jumlah Pria dan Wanita berimbang"
-  */
-  let komentar = null;
+    /*
+        TODO 4: Buatlah sebuah variabel bernama "komentar"
+          yang akan mengomentari apakah lebih banyak Pria atau Wanita
+          
+          Apabila Pria lebih banyak dari Wanita, maka komentar akan berisi:
+            "Jumlah Pria lebih banyak dari Wanita"
+          Apabila Wanita lebih banyak dari Pria, maka komentar akan berisi:
+            "Jumlah Wanita lebih banyak dari Pria"
+          Apabila imbang, maka komentar akan berisi:
+            "Jumlah Pria dan Wanita berimbang"
+      */
+    let komentar = null;
+    if (jumlahPria == jumlahWanita) {
+        komentar = "Jumlah Pria dan Wanita berimbang";
+    } else {
+        if (jumlahPria > jumlahWanita) {
+            komentar = "Jumlah Pria lebih banyak dari Wanita";
+        } else {
+            komentar = "Jumlah Wanita lebih banyak dari Pria";
+        }
+    }
 
-  // ! JANGAN DIMODIFIKASI
-  return {
-    hasilLooping,
-    jumlahPria,
-    jumlahWanita,
-    komentar,
-  };
+    // ! JANGAN DIMODIFIKASI
+    return {
+        hasilLooping,
+        jumlahPria,
+        jumlahWanita,
+        komentar,
+    };
 }
 
 function main(data) {
-  const hasil = lakukanLooping(data || arrayObjectPegawai);
+    const hasil = lakukanLooping(data || arrayObjectPegawai);
 
-  console.log(hasil.hasilLooping);
-  console.log(hasil.jumlahPria);
-  console.log(hasil.jumlahWanita);
+    console.log(hasil.hasilLooping);
+    console.log(hasil.jumlahPria);
+    console.log(hasil.jumlahWanita);
 
-  return hasil;
+    return hasil;
 }
 
 main(arrayObjectPegawai);
